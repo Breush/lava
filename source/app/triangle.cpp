a479924c40dc80f60ee204ce1330f0c2b0cee42b
Alexis Breust <alexis.breust@gmail.com>
2017-04-29 11:54:29 +0200

    chamber clean-up

--- source/app/triangle.cpp
+++ source/app/triangle.cpp
@@ -1,22 +1,23 @@
 #include "./vulkan-example.hpp"
 
-int main(void) {
+int main(void)
+{
     lava::Window window(lava::VideoMode(800, 600), "My window");
 
-	VulkanExample vulkanExample(window);
-	vulkanExample.initVulkan();
-	vulkanExample.setupWindow();
-	vulkanExample.initSwapchain();
-	vulkanExample.prepare();
+    VulkanExample vulkanExample(window);
+    vulkanExample.initVulkan();
+    vulkanExample.setupWindow();
+    vulkanExample.initSwapchain();
+    vulkanExample.prepare();
 
     while (window.isOpen()) {
         lava::Event event;
         while (window.pollEvent(event)) {
-        	vulkanExample.handleLavaEvent(event);
+            vulkanExample.handleLavaEvent(event);
         }
 
         vulkanExample.render();
     }
 
-	return 0;
+    return 0;
 }
--- source/chamber/Unix/MutexImpl.hpp
+++ source/chamber/Unix/MutexImpl.hpp
@@ -1,83 +1,24 @@
-////////////////////////////////////////////////////////////
-//
-// SFML - Simple and Fast Multimedia Library
-// Copyright (C) 2007-2017 Laurent Gomila (laurent@sfml-dev.org)
-//
-// This software is provided 'as-is', without any express or implied warranty.
-// In no event will the authors be held liable for any damages arising from the use of this software.
-//
-// Permission is granted to anyone to use this software for any purpose,
-// including commercial applications, and to alter it and redistribute it freely,
-// subject to the following restrictions:
-//
-// 1. The origin of this software must not be misrepresented;
-//    you must not claim that you wrote the original software.
-//    If you use this software in a product, an acknowledgment
-//    in the product documentation would be appreciated but is not required.
-//
-// 2. Altered source versions must be plainly marked as such,
-//    and must not be misrepresented as being the original software.
-//
-// 3. This notice may not be removed or altered from any source distribution.
-//
-////////////////////////////////////////////////////////////
+#pragma once
 
-#ifndef SFML_MUTEXIMPL_HPP
-#define SFML_MUTEXIMPL_HPP
-
-////////////////////////////////////////////////////////////
-// Headers
-////////////////////////////////////////////////////////////
 #include <lava/chamber/NonCopyable.hpp>
 #include <pthread.h>
 
+namespace lava::priv {
 
-namespace lava
-{
-namespace priv
-{
-////////////////////////////////////////////////////////////
-/// \brief Unix implementation of mutexes
-////////////////////////////////////////////////////////////
-class MutexImpl : NonCopyable
-{
-public:
+    /// \brief Unix implementation of mutexes
 
-    ////////////////////////////////////////////////////////////
-    /// \brief Default constructor
-    ///
-    ////////////////////////////////////////////////////////////
-    MutexImpl();
+    class MutexImpl : NonCopyable {
+    public:
+        MutexImpl();
+        ~MutexImpl();
 
-    ////////////////////////////////////////////////////////////
-    /// \brief Destructor
-    ///
-    ////////////////////////////////////////////////////////////
-    ~MutexImpl();
+        /// \brief Lock the mutex
+        void lock();
 
-    ////////////////////////////////////////////////////////////
-    /// \brief Lock the mutex
-    ///
-    ////////////////////////////////////////////////////////////
-    void lock();
+        /// \brief Unlock the mutex
+        void unlock();
 
-    ////////////////////////////////////////////////////////////
-    /// \brief Unlock the mutex
-    ///
-    ////////////////////////////////////////////////////////////
-    void unlock();
-
-private:
-
-    ////////////////////////////////////////////////////////////
-    // Member data
-    ////////////////////////////////////////////////////////////
-    pthread_mutex_t m_mutex; ///< pthread handle of the mutex
-};
-
-} // namespace priv
-
-} // namespace lava
-
-
-#endif // SFML_MUTEXIMPL_HPP
+    private:
+        pthread_mutex_t m_mutex; ///< pthread handle of the mutex
+    };
+}
