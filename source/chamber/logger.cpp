8be62be34e9051a4cc08c6ce25c057f05dc8377b
Alexis Breust <alexis.breust@gmail.com>
2017-08-04 20:08:38 +0200

    Added clever spacing during log

--- source/chamber/logger.cpp
+++ source/chamber/logger.cpp
@@ -1,8 +1,22 @@
 #include <lava/chamber/logger.hpp>
 
+#include <lava/chamber/math.hpp>
+#include <lava/chamber/string-tools.hpp>
+
 using namespace lava;
 
 chamber::Logger chamber::logger;
+
+namespace {
+    std::string spacing(const std::string& string)
+    {
+        static const std::string space(" ");
+        static uint32_t maxStringLength = 0u;
+        uint32_t stringLength = string.size();
+        maxStringLength = std::max(maxStringLength, stringLength);
+        return space * (maxStringLength - stringLength);
+    }
+}
 
 using namespace lava::chamber;
 
@@ -19,20 +33,20 @@ LoggerStream& Logger::log()
 LoggerStream& Logger::info(const std::string& category)
 {
     m_stream.autoExit(false);
-    m_stream.prefixString("\e[1m[" + category + "] \e[32m");
+    m_stream.prefixString("\e[1m[" + category + "] \e[32m" + spacing(category));
     return m_stream;
 }
 
 LoggerStream& Logger::warning(const std::string& category)
 {
     m_stream.autoExit(false);
-    m_stream.prefixString("\e[1m[" + category + "] \e[33m/!\\ ");
+    m_stream.prefixString("\e[1m[" + category + "] \e[33m/!\\ " + spacing(category));
     return m_stream;
 }
 
 LoggerStream& Logger::error(const std::string& category)
 {
     m_stream.autoExit(true);
-    m_stream.prefixString("\e[1m[" + category + "] \e[31m/!\\ /!\\ ");
+    m_stream.prefixString("\e[1m[" + category + "] \e[31m/!\\ /!\\ " + spacing(category));
     return m_stream;
 }
