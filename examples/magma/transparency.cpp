3cddb6f70b188a771b62d0c48c37d428ee0dfd27
Alexis Breust <abreust@wanadev.fr>
2017-09-07 13:44:42 +0200

    Added transparency example

---
+++ examples/magma/transparency.cpp
@@ -0,0 +1,39 @@
+/**
+ * Shows how transparency works using magma rendering-engine.
+ */
+
+#include "../ashe.hpp"
+
+using namespace lava;
+
+int main(void)
+{
+    ashe::Application app("ashe - magma | Transparency");
+
+    // Blue mesh
+    {
+        auto& blueMaterial = app.scene().make<magma::RmMaterial>();
+        blueMaterial.baseColor({0u, 0u, 255u, 120u}, 1u, 1u, 4u);
+
+        auto& blueMesh = app.scene().make(magma::makers::planeMeshMaker({1, 1}));
+        // blueMesh.rotate(math::PI_2);
+        blueMesh.material(blueMaterial);
+    }
+
+    // Red mesh
+    {
+        auto& redMaterial = app.scene().make<magma::RmMaterial>();
+        redMaterial.baseColor({255u, 0u, 0u, 120u}, 1u, 1u, 4u);
+
+        auto& redMesh = app.scene().make(magma::makers::planeMeshMaker({1, 1}));
+        redMesh.positionAdd({-0.25f, 0.f, 0.f});
+        redMesh.material(redMaterial);
+    }
+
+    app.camera().position({2.f, 2.f, 2.f});
+    app.camera().target({0.f, 0.f, 0.f});
+
+    app.run();
+
+    return EXIT_SUCCESS;
+}
--- examples/premake5.lua
+++ examples/premake5.lua
@@ -16,3 +16,8 @@ project "magma-scenes-and-windows"
     kind "WindowedApp"
     files "magma/scenes-and-windows.cpp"
     useMagma()
+
+project "magma-transparency"
+    kind "WindowedApp"
+    files "magma/transparency.cpp"
+    useMagma()
