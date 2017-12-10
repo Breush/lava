a0442a906774d861cbfa35d822e79172df056a1c
A. Breust <alexis.breust@gmail.com>
2017-12-10 10:31:18 +0100

    Improved run.sh by only compiling necessary libs

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,23 +5,33 @@
 
 cd $(dirname "$0")/..
 
+# Find program indentified by the make alias
+MAKE_PROGRAM="make"
+if [ `uname -o` == "Msys" ]; then
+    MAKE_PROGRAM="mingw32-make"
+fi
+
 # These are needed for Vulkan validation layers to work
 export LD_LIBRARY_PATH=`pwd`/external/lib:${LD_LIBRARY_PATH}
 export VK_LAYER_PATH=`pwd`/external/etc/explicit_layer.d
 
-./scripts/setup.sh && ./scripts/build.sh
+# Find a file that match the name
+EXECUTABLES=$(find ./build/debug/ -type f -executable -name "*$1*" -not -name "*.so"  -not -name "*.dll")
+EXECUTABLES=($EXECUTABLES)
+EXECUTABLES_COUNT=${#EXECUTABLES[@]}
 
-if [ $? -eq 0 ]; then
-    # Find a file that match the name
-    EXECUTABLES=$(find ./build/debug/ -type f -executable -name "*$1*" -not -name "*.*")
-    EXECUTABLES=($EXECUTABLES)
-    EXECUTABLES_COUNT=${#EXECUTABLES[@]}
+if [ ${EXECUTABLES_COUNT} -eq 1 ]; then
+    # Build
+    EXECUTABLE=${EXECUTABLES[0]}
+    MAKE_TARGET=$(basename "${EXECUTABLE}" | cut -d '.' -f 1)
+    
+    echo -e "\e[35mBuilding ${MAKE_TARGET}...\e[39m"
+    
+    ${MAKE_PROGRAM} ${MAKE_TARGET}
 
-    if [ ${EXECUTABLES_COUNT} -eq 1 ]; then
-        EXECUTABLE=${EXECUTABLES[0]}
-        echo -en "\e[33m"
-        echo "Running ${EXECUTABLE}..."
-
+    # Run
+    if [ $? -eq 0 ]; then
+        echo -e "\e[35mRunning ${EXECUTABLE}..."
         if [ "$2" == "debug" ]; then
             echo "... in debug mode."
             EXECUTABLE="gdb --quiet --directory=./external/source/vulkan/layers -ex run ${EXECUTABLE}*"
@@ -30,13 +40,13 @@ if [ $? -eq 0 ]; then
 
         ${EXECUTABLE}
     else
-        echo -en "\e[33m"
-        echo "Found ${EXECUTABLES_COUNT} executables."
-        for EXECUTABLE in ${EXECUTABLES[@]}; do
-            echo ${EXECUTABLE}
-        done
-        echo -en "\e[39m"
+        echo -e "\e[31m\nError during build...\e[39m"
     fi
 else
-    echo -e "\e[31m\nError during setup or build...\e[39m"
+    echo -en "\e[33m"
+    echo "Found ${EXECUTABLES_COUNT} executables."
+    for EXECUTABLE in ${EXECUTABLES[@]}; do
+        echo ${EXECUTABLE}
+    done
+    echo -en "\e[39m"
 fi
