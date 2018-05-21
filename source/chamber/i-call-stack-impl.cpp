8ba6ef4f00506e02bb2e85871e35718f817e9e88
Alexis Breust <alexis.breust@gmail.com>
2018-05-21 14:46:16 +0200

    Improved callstack logging with colors

--- source/chamber/i-call-stack-impl.cpp
+++ source/chamber/i-call-stack-impl.cpp
@@ -23,7 +23,25 @@ std::string ICallStackImpl::toString() const
         std::stringstream str;
         str << entry.file << ":" << entry.line;
         const auto spacing = std::string(" ") * (spacingLength - str.str().size() + 1);
-        os << "\e[1m" << entry.file << "\e[2m:" << entry.line << "\e[0m" << spacing << entry.function << std::endl;
+        os << "\e[1m" << entry.file << "\e[2m:" << entry.line << "\e[0m" << spacing;
+
+        // Parse and color the function
+        const auto leftParenthesis = entry.function.find_first_of('(');
+        auto functionNameColon = entry.function.find_last_of(':', leftParenthesis) + 1u;
+        if (functionNameColon == std::string::npos) functionNameColon = 0u;
+
+        std::string functionPrefix = entry.function.substr(0u, functionNameColon);
+        std::string functionName = entry.function.substr(functionNameColon, leftParenthesis - functionNameColon);
+        std::string arguments = "";
+        if (leftParenthesis != std::string::npos) {
+            arguments = entry.function.substr(leftParenthesis);
+        }
+
+        os << functionPrefix << "\e[94m" << functionName;
+        if (!arguments.empty()) {
+            os << "\e[37m" << arguments;
+        }
+        os << "\e[0m" << std::endl;
     }
 
     return os.str();
